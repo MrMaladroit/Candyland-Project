8116d70679dbd2dea6c60220af57bdcb6d6479ba
MrMaladroit <jamesbeckjr@gmail.com>
2019-07-01 11:18:16 -0400

    WIP Card Scriptable Object
    Started adding the logic for cards using Scriptable Objects
    Card takes an image, TileType and bool IsDouble
    
    TODO Refactor singleOrDouble in MoveCalculator.CalculateMove to bool IsDouble and refactor method body to assign singleOrDouble

---
+++ Candyland/Assets/Scripts/Scriptable Objects/Card.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject
+{
+    public Image cardImage;
+    public TileType TileType;
+    public bool IsDouble;
+}
