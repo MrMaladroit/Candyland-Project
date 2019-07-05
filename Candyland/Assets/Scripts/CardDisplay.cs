7e35a465a2957de766a694d20b99cb03781c9bce
MrMaladroit <jamesbeckjr@gmail.com>
2019-07-05 17:46:05 -0400

    Basic Card Art Functionality
    Added basic card artwork for playing cards
    Added CardDisplay script for basic card graphic setup

---
+++ Candyland/Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Card Card;
+    public SpriteRenderer CardFrontArt;
+    public SpriteRenderer CardBackArt;
+
+    private bool isDouble;
+    private TileType tileType;
+
+    private void Start()
+    {
+        CardFrontArt.sprite = Card.CardFrontImage;
+        CardBackArt.sprite = Card.CardBackImage;
+        isDouble = Card.IsDouble;
+        tileType = Card.TileType;
+    }
+}
--- Candyland/Assets/Scripts/Scriptable Objects/Card.cs
+++ Candyland/Assets/Scripts/Scriptable Objects/Card.cs
@@ -3,7 +3,8 @@
 [CreateAssetMenu(fileName = "New Card", menuName = "Card")]
 public class Card : ScriptableObject
 {
-    public Sprite cardImage;
+    public Sprite CardFrontImage;
+    public Sprite CardBackImage;
     public TileType TileType;
     public bool IsDouble;
 }
