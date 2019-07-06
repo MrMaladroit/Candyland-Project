e94384967d78a52c0649658e33ba9989f5abb670
MrMaladroit <jamesbeckjr@gmail.com>
2019-07-05 20:09:03 -0400

    WIP Player Turn Controller

--- Candyland/Assets/Scripts/Player.cs
+++ Candyland/Assets/Scripts/Player.cs
@@ -4,12 +4,24 @@ public class Player : MonoBehaviour
 {
     public static int PlayerCount = 0;
     public bool IsHuman = false;
-    public bool IsCurrentPlayer { get { return isCurrentPlayer; } private set { isCurrentPlayer = value; } }
+    public bool IsCurrentPlayer { get; set; }
+
 
     private bool isCurrentPlayer = false;
+    private PieceMover pieceMover;
 
     private void Start()
     {
         PlayerCount++;
+        pieceMover = GetComponent<PieceMover>();
+        pieceMover.enabled = false;
+        PieceMover.OnMoveFinished += SetPieceMoverActivity;
     }
+
+    private void SetPieceMoverActivity()
+    {
+        pieceMover.enabled = isCurrentPlayer;
+    }
+
+
 }
---
+++ Candyland/Assets/Scripts/PlayerTurnController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerTurnController : MonoBehaviour
+{
+    [SerializeField] Player[] players;
+    private int turnCounter = 0;
+        
+    private void Start()
+    {
+        PieceMover.OnMoveFinished += PassTurn;
+    }
+
+    private void PassTurn()
+    {
+        FlipPlayerIsCurrentPlayerProp();
+        turnCounter++;
+        FlipPlayerIsCurrentPlayerProp();
+    }
+
+    private void FlipPlayerIsCurrentPlayerProp()
+    {
+        players[turnCounter % Player.PlayerCount].IsCurrentPlayer = !players[turnCounter % Player.PlayerCount].IsCurrentPlayer;
+    }
+
+}
